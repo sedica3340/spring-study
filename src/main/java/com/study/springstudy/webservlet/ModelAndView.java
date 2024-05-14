23aaed285c6f8002cb0940092b6d4bdea0a35d83
sedica3340 <sedica3340@gmail.com>
2024-05-14 17:27:51 +0900

    af

--- src/main/java/com/study/springstudy/webservlet/ModelAndView.java
+++ src/main/java/com/study/springstudy/webservlet/ModelAndView.java
@@ -5,18 +5,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-// 모델과 뷰를 통합하는 역할
 public class ModelAndView {
 
-    private View view;      // 화면처리
-    private Model model;    // 화면에 필요한 데이터 처리
+    private View view;   // 화면 처리
+    private Model model; // 화면에 필요한 데이터 처리
 
     public ModelAndView(String viewName) {
         this.view = new View(viewName);
+        this.model = new Model();
     }
 
-    // 화면 렌더링 기능 (포워딩 리다이렉트)
-
+    // 화면 렌더링 기능 (포워딩 or 리다이렉트)
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         this.view.render(request, response);
     }
