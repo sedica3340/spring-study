2437f96f9e4d74705508f9bd53ef9cda3a34200f
sedica3340 <sedica3340@gmail.com>
2024-05-16 17:21:00 +0900

    af

---
+++ src/main/java/com/study/springstudy/springmvc/chap03/controller/ScoreController.java
@@ -0,0 +1,86 @@
+package com.study.springstudy.springmvc.chap03.controller;
+
+
+import com.study.springstudy.springmvc.chap03.dto.ScorePostDto;
+import com.study.springstudy.springmvc.chap03.entity.Score;
+import com.study.springstudy.springmvc.chap03.repository.ScoreJdbcRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/score")
+public class ScoreController {
+
+    // 의존객체 설정
+    private ScoreJdbcRepository repository = new ScoreJdbcRepository();
+
+
+    /*
+    # 요청 URL
+    1. 학생 성적정보 등록화면을 보여주고 및 성적정보 목록조회 처리
+    - /score/list : GET
+*/
+    @GetMapping("/list")
+    public String list(Model model) {
+        System.out.println("/score/list : GET!");
+
+        List<Score> scoreList = repository.findAll();
+
+        model.addAttribute("sList", scoreList);
+
+        return "score/score-list";
+    }
+
+
+    /*
+    2. 성적 정보 등록 처리 요청
+    - /score/register : POST
+    */
+    @PostMapping("/register")
+    public String register(ScorePostDto dto) {
+        System.out.println("/score/register : POST!");
+        System.out.println(dto);
+
+
+        // 데이터베이스 저장 위임..
+        Score score = new Score(dto);
+
+        repository.save(score);
+
+        return "redirect:/score/list";
+    }
+    /*
+    3. 성적정보 삭제 요청
+    - /score/remove : POST
+    */
+    @PostMapping("/remove")
+    public String remove() {
+        System.out.println("score/remove : POST!");
+        return "";
+    }
+
+
+    /*
+    4. 성적정보 상세 조회 요청
+    - /score/detail : GET
+ */
+    @GetMapping("/detail")
+    public String detail(Long stuNum, Model model) {
+        System.out.println("score/detail : GET!");
+
+        // 1. 상세조회를 원하는 학번을 읽기
+        // 2. DB에 상세조회 요청
+        Score score = repository.findOne(stuNum);
+        // 3. DB에서 조회한 회원정보 JSP에게 전달
+        String rank = repository.ranking(score);
+        model.addAttribute("s", score);
+        model.addAttribute("rank", rank);
+
+        return "score/score-detail";
+    }
+}
---
+++ src/main/java/com/study/springstudy/springmvc/chap03/repository/ScoreJdbcRepository.java
@@ -0,0 +1,117 @@
+package com.study.springstudy.springmvc.chap03.repository;
+
+import com.study.springstudy.springmvc.chap03.entity.Score;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ScoreJdbcRepository implements ScoreRepository {
+    private String url = "jdbc:mariadb://localhost:3306/spring5";
+    private String username = "root";
+    private String password = "mariadb";
+
+    public ScoreJdbcRepository() {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public boolean save(Score score) {
+
+        try (Connection conn = connect()) {
+
+            String sql = "INSERT INTO tbl_score " +
+                    "(stu_name, kor, eng, math, total, average, grade) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, score.getStuName());
+            pstmt.setInt(2, score.getKor());
+            pstmt.setInt(3, score.getEng());
+            pstmt.setInt(4, score.getMath());
+            pstmt.setInt(5, score.getTotal());
+            pstmt.setDouble(6, score.getAverage());
+            pstmt.setString(7, score.getGrade().toString());
+
+            pstmt.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public List<Score> findAll() {
+        List<Score> scoreList = new ArrayList<>();
+        try (Connection conn = connect()) {
+            String sql = "SELECT * FROM tbl_score";
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                Score s = new Score(rs);
+                scoreList.add(s);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return scoreList;
+    }
+
+    @Override
+    public Score findOne(Long stuNum) {
+
+        Score score = null;
+        try (Connection conn = connect()) {
+            String sql = "SELECT * FROM tbl_score WHERE stu_Num = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setLong(1, stuNum);
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            score = new Score(rs);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return score;
+    }
+
+    @Override
+    public String ranking(Score score) {
+        List<Score> scoreList = findAll();
+        List<Score> rankingList = scoreList.stream()
+                .sorted(Comparator.comparing(Score::getAverage).reversed())
+                .collect(Collectors.toList());
+        int rank = -1;
+        for (int i = 0; i < rankingList.size(); i++) {
+            if (rankingList.get(i).getStuNum() == (score.getStuNum())) {
+                rank = i + 1;
+            }
+        }
+        String ranking = rank + "/" + rankingList.size();
+
+
+        return ranking;
+    }
+
+    private Connection connect() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+}
